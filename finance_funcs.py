5d36bfba5cf608dd5ceeff943a51ac85e8e85076
Justin Montgomery <montgomerycodepro@gmail.com>
2020-06-28 10:37:53 -0400

    using a new API to aquire financial data

--- finance_funcs.py
+++ finance_funcs.py
@@ -1,21 +1,29 @@
 import pandas as pd
 import pprint
-pp=pprint.PrettyPrinter(indent=4)
 import numpy as np
-from alpha_vantage.timeseries import TimeSeries
+from iexfinance.stocks import get_historical_data
+from iexfinance.stocks import Stock
 import os
+from datetime import datetime
+
+# global variables one for the start and end parameters as well as the auth token needed to make API calls. 
+beginning = datetime(2015,1,1)
+finish = datetime.now()
+access = os.getenv('IEX_Token')
 
 
-def single_adj_close(ticker):
-    ts = TimeSeries(key=os.environ['AV_API_KEY'],output_format='pandas', outputsize='full')
-    data, meta_data = ts.get_daily_adjusted(ticker)
-    return (data['5. adjusted close'])
+# sec stands for security. finance term for a tradeable asset. a stock is a security. 
+# need to pass in the stocks "ticker" aka the abbrevaition on trading platforms. 
+def single_adj_close(sec):
+    data = get_historical_data(sec, start=beginning,end=finish, output_format='pandas',token=access,)['close']
+    return data
+
     
 
-def simple_return(ticker):
-    data = single_adj_close(ticker)
-    s_return = data / data.shift(1) - 1
-    stock_daily_return = s_return.mean()
+def simple_return(sec):
+    data = single_adj_close(sec)
+    simple_return = data / data.shift(1) - 1
+    stock_daily_return = simple_return.mean()
     return print(f'Daily return over past 4 months {stock_daily_return * 100 :.3f}%')
 
-single_adj_close('MSFT')
+print(single_adj_close('AAPL'))
