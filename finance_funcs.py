94b885c0f055b5720128c07fddffcafbe4b292ac
Justin Montgomery <montgomerycodepro@gmail.com>
2020-07-01 13:37:51 -0400

    created more helper functions to solve financial equations, added sharpe function, and added more comments and spacing to clean up code

--- finance_funcs.py
+++ finance_funcs.py
@@ -4,7 +4,7 @@ import pandas_datareader.data as wb
 import requests
 from bs4 import BeautifulSoup
 
-# all figures returned are percentages
+# some figures are returned as percentages. But other values are not because the return values are needed to calcuate other functions. 
 # everything is calculated yearly. multiple by 250 because on average thats how many days the markets are open. 
 
 
@@ -16,6 +16,7 @@ yahoo = 'yahoo'
 # all data is gathered from the Yahoo Finance through the pandas_datareader package.
 # sec stands for security. finance term for a tradeable asset. a stock is a security. 
 # need to pass in the stocks "ticker" aka the abbrevaition on pretty much all trading platforms. 
+"""Helper functions for calculations"""
 
 def data_reader(*securities):
     data = pd.DataFrame()
@@ -23,10 +24,15 @@ def data_reader(*securities):
         data[item] = wb.DataReader(item,start=start,data_source=yahoo)['Adj Close']
     return data
 
-
+def logged(shifted_data):
+    return np.log(shifted_data)
 def shifted(data):
     # divides each entry in the dataframe by the next entry.
     return (data / data.shift(1))
+def error(str):
+    return RuntimeError(str)
+
+"""Financial Functions"""
 
 # annual simple return of the passed in security. 
 def simple_return(sec):
@@ -39,7 +45,7 @@ def simple_return(sec):
 def log_return(sec):
     # log returns are better for looking at one security over a period of time.
     data = data_reader(sec)
-    log_return = np.log(shifted(data))
+    log_return = logged(shifted(data))
     log_annual_return = log_return.mean() * 250
     return print(f'{log_annual_return[sec] * 100:.4f}%')
 
@@ -49,10 +55,9 @@ def log_return(sec):
 # Volatility shows how often a security goes above or below its standard deviation. 
 def sec_volatility(*sec):
     data = data_reader(*sec)
-    sec_returns = np.log(shifted(data))
+    sec_returns = logged(shifted(data))
     annual_volatility = sec_returns.std() * 250 ** .5
     return print(f'{annual_volatility * 100}')
-
 
 # indices are clusters of securities in one place that are usually market indicators.
 # 3 most heard of index funds are dowjones, s&p 500, & nasdaq. 
@@ -65,7 +70,6 @@ def market_indicators(*indices):
     id_return = (shifted(data)-1)
     annual_id_return = id_return.mean() * 250
     return print(f'{annual_id_return * 100}')
-
 
 def portfolio_return(weights,*stocks):
     # function calculates the return of your portfolio based on the stocks you pass in
@@ -85,14 +89,14 @@ def portfolio_return(weights,*stocks):
 # returns the volatility of your whole portfolio. 
 def portfolio_volatility(weights,*sec):
     data = data_reader(*sec)
-    sec_returns = np.log(shifted(data))
+    sec_returns = logged(shifted(data))
     if len(weights) == len(sec) and sum(weights) == 1:
         annual_covariance = sec_returns.cov() * 250
         array_weights = np.array(weights)
         pfolio_volatilty = (np.dot(array_weights.T, np.dot(sec_returns.cov() * 250, array_weights))) ** 0.5
         return print(f'{pfolio_volatilty * 100:.4f} %')
     else:
-         raise RuntimeError('Make sure your weights add to 1 and the lengths of sec and weights you are passing in are the same.')
+         raise error('Make sure your weights add to 1 and the lengths of sec and weights you are passing in are the same.')
 
 # weights = [.5,.4,]
 # portfolio_volatility(weights,'AAPL','MSFT')
@@ -108,15 +112,15 @@ def portfolio_volatility(weights,*sec):
 # Beta describes the activity of a security's returns as it responds to swings in the market. 
 def beta(sec,index):
     if index not in ['^GSPC','^IXIC','^DJI']:
-        raise RuntimeError('use one of the three major indices in the US')
+        raise error('use one of the three major indices in the US')
     else:
         data = data_reader(sec,index)
-        returns = np.log(shifted(data))
+        returns = logged(shifted(data))
         cov = returns.cov() * 250
         cov_with_mkt = cov.iloc[0,1]
         market_var = returns[index].var() * 250
         sec_beta = cov_with_mkt / market_var
-    return f'{sec_beta:.4f}'
+    return sec_beta
 
 # function to webscrape the ten year bond yield and use it as the risk free rate in the beta calculation. 
 def get_risk_free_rate():
@@ -127,20 +131,31 @@ def get_risk_free_rate():
     yield_span = results.find(class_='series-meta-observation-value')
     for rate in yield_span:
        ten_year_bond_yield = float(rate)
-    return ten_year_bond_yield
+    return (ten_year_bond_yield / 100)
+
 
 # calculates the expected return of a stock. 
 # for this function it calculates the beta of the stock based off of one of the three major indicies in the US. 
 def expected_return(sec,index):
     if index not in ['^GSPC','^IXIC','^DJI']:
-        raise RuntimeError('use one of the three major indices in the US')
+        raise error('use one of the three major indices in the US')
     else:
         risk_free_rate = get_risk_free_rate()
         sec_beta = float(beta(sec,index))
-    return f'{(risk_free_rate / 100) + (sec_beta * .05) * 100:.4f}%'
+    return risk_free_rate + sec_beta * .05
+
+print(round(expected_return('MSFT','^GSPC'),4))
+
 
 def sharpe_ratio(sec,index):
-    pass
+    if index not in ['^GSPC','^IXIC','^DJI']:
+       raise error('use one of the three major indices in the US')
+    else:
+        data = data_reader(sec,index)
+        returns = logged(shifted(data))
+        sharpe = (expected_return(sec,index) - get_risk_free_rate()) / (returns[sec].std() * 250 ** .05)
+    return sharpe    
+
 
 
 
